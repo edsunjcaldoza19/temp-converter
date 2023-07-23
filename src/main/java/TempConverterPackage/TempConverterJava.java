62362c067182cf580a4c298fc973a6cb9fbe18de
EdsunCaldoza <edsunjcaldoza@gmail.com>
2023-07-23 11:48:25 +0800

    First Commit

---
+++ src/main/java/TempConverterPackage/TempConverterJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package TempConverterPackage;
+
+/**
+ *
+ * @author EdSun
+ */
+public class TempConverterJava {
+
+    public static void main(String[] args) {
+        new TempConverter().setVisible(true);
+    }
+}
